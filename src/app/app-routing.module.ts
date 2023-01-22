e54645803c1164dc829f78e2af506dc528ea9da2
Thanaphol <thanaphol.iqtha@gmail.com>
2023-01-23 03:53:39 +0700

    update page and little more css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './web/login/login.component';
 import { HomeComponent } from './web/home/home.component';
 import { DonorComponent } from './web/donor/donor.component';
 import { ProfileEditComponent } from './web/profile/edit/edit.component';
+import { BookComponent } from './web/book/book.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'donor', component: DonorComponent },
-  { path: 'profile/edit', component: ProfileEditComponent }
-
-];
+  { path: 'profile/edit', component: ProfileEditComponent },
+  { path: 'booking', component: BookComponent },
+  { path: 'status', component: HomeComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
